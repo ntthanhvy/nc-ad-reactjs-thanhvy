aa35252a50d7d6f32c1d9f2e693ff35eaf6c9e3c
ntthanhvy <nguyenthithanhvyjps15@gmail.com>
2020-07-01 22:08:38 +0700

    lab 1

---
+++ Day1/lab1.ts
@@ -0,0 +1,13 @@
+let str: string = "this is a string";
+let num: number = 1;
+let b: boolean = false;
+
+let arr: number[] = [];
+let obj: { id: number; code: string };
+
+arr = [1, 2, 3];
+obj = { id: 1, code: "abcd12345" };
+
+// type-err
+str  = 0;
+arr= [1, "2"],
